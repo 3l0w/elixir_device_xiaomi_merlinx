64192a295dccf409fbd5c7effcc2e6456141caac
Gwilherm Folliot <gwilherm55fo@gmail.com>
2023-02-24 21:15:00 +0100

    merlinx: Elixir bring up

--- aosp_merlinx.mk
+++ aosp_merlinx.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/merlinx/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_merlinx
+PRODUCT_NAME := aosp_merlinx
 PRODUCT_DEVICE := merlinx
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -24,5 +24,13 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="merlin-user 11 RP1A.200720.011 V12.5.4.0.RJOMIXM release-keys"
+
+TARGET_BOOT_ANIMATION_RES := 1080
+# Elixir Stuff
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 BUILD_FINGERPRINT := Redmi/merlin/merlin:11/RP1A.200720.011/V12.5.4.0.RJOMIXM:user/release-keys
---
+++ vendorsetup.sh
@@ -0,0 +1,25 @@
+## common tree
+
+git clone -b lineage-20 https://github.com/kaikeEXE/android_device_xiaomi_mt6768-common device/xiaomi/mt6768-common
+
+##kernel tree
+
+git clone -b lineage-20 https://github.com/mt6768-dev/android_kernel_xiaomi_mt6768 kernel/xiaomi/mt6768
+
+##hardware
+
+git clone -b lineage-20 https://github.com/mt6768-dev/android_hardware_mediatek hardware/mediatek
+git clone -b lineage-20.0 https://github.com/LineageOS/android_hardware_lineage_compat.git hardware/lineage/compat
+
+##fm radio
+
+git clone -b lineage-20 https://github.com/mt6768-dev/android_packages_apps_MtkFMRadio packages/apps/MtkFMRadio
+
+##mtk sepolicy
+
+git clone -b lineage-20 https://github.com/mt6768-dev/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+
+##vendor
+
+git clone -b lineage-20 https://github.com/mt6768-dev/proprietary_vendor_xiaomi_merlinx.git vendor/xiaomi/merlinx
+git clone -b lineage-20 https://github.com/mt6768-dev/proprietary_vendor_xiaomi_mt6768-common.git vendor/xiaomi/mt6768-common
